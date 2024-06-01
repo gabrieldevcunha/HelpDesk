ba0d8d3853e35d33eed05e8f6c4cbde56c1ee0c6
Gabriel Cunha <gabriel.albe@hotmail.com>
2024-06-01 12:05:36 -0300

    Feature/#DomainCliente

---
+++ src/main/java/com/project/helpdesk/domain/Cliente.java
@@ -0,0 +1,32 @@
+package com.project.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+}
